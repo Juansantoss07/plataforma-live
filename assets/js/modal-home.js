e5e2e5e33b7a6180cd2e57db24e6c119d14f5890
Juansantoss07 <juansantosscunha@gmail.com>
2021-11-27 12:45:20 -0300

    Fix: update

---
+++ assets/js/modal-home.js
@@ -0,0 +1,33 @@
+ 
+  var modal = document.getElementById("myModal1");
+  var btn = document.getElementById("myBtn1");
+  var span = document.getElementsByClassName("close")[1];
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  var modal2 = document.getElementById("myModal2");
+  var btn2 = document.getElementById("myBtn2");
+  var span = document.getElementsByClassName("close")[2];
+  btn2.onclick = function() {
+    modal2.style.display = "block";
+  }
+
+  span.onclick = function() {
+    modal2.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal2) {
+      modal2.style.display = "none";
+    }
+  };
+
